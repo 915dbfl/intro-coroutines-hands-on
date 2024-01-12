8db7a40f7b6a2d2d4f67e44436dda7947036b851
915dbfl <dbfl00915@naver.com>
2024-01-12 16:52:28 +0900

    [FEAT] channel 기능 구현하기

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -11,6 +11,30 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: emptyList()
+
+        val allUsers = mutableListOf<User>()
+        val channel = Channel<List<User>>()
+
+        for (repo in repos) {
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+
+                channel.send(users)
+            }
+        }
+
+        repeat(repos.size) {index ->
+            val users = channel.receive()
+            allUsers += users
+            val isCompleted = index == repos.lastIndex
+            updateResults(allUsers.aggregate(), isCompleted)
+        }
     }
 }
