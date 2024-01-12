df4228bdbf819dec4d258a4d7cfefd98a75f09b7
915dbfl <dbfl00915@naver.com>
2024-01-12 16:41:13 +0900

    [FEAT] contribution aggregation 기능 추가

--- src/tasks/Aggregation.kt
+++ src/tasks/Aggregation.kt
@@ -14,5 +14,11 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> {
+    groupBy { it.login } // login 정보로 group by, login을 기준으로 묶었기 때문에 key = login, value = 그룹으로 묶여진 값들(유저들)
+        .map { (login, users) -> User( login, users.sumOf { it.contributions }) } // 하나의 로그인에 대한 전체 contribution 구해 하나의 유저로 만들기
+        .sortedByDescending { it.contributions }
+
+    return this
+    TODO()
+}
