80725b4cffe34a621fa62f7af3ab85c30cb3a1c8
915dbfl <dbfl00915@naver.com>
2024-01-12 16:45:37 +0900

    [FEAT] callBack 기능 수정

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -5,25 +5,31 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicInteger
 
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
+    // 전체 contributors를 가져왔는지 확인하기 위한 counter
+    // gerRepoContributorsCall이 여러 개의 Thread에 동시에 호출 될 수 있기 때문에 : AtomicInteger 사용
+    val counter = AtomicInteger(0)
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
+        val countDownLatch = CountDownLatch(repos.size)
         val allUsers = mutableListOf<User>()
         for (repo in repos) {
-            service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
-                logUsers(repo, responseUsers)
-                val users = responseUsers.bodyList()
-                allUsers += users
-            }
+            service.getRepoContributorsCall(req.org, repo.name)
+                .onResponse { responseUsers ->
+                    logUsers(repo, responseUsers)
+                    val users = responseUsers.bodyList()
+                    allUsers += users
+                    countDownLatch.countDown()
+                }
         }
-        // TODO: Why this code doesn't work? How to fix that?
+        countDownLatch.await()
         updateResults(allUsers.aggregate())
     }
 }
-
 inline fun <T> Call<T>.onResponse(crossinline callback: (Response<T>) -> Unit) {
     enqueue(object : Callback<T> {
         override fun onResponse(call: Call<T>, response: Response<T>) {
